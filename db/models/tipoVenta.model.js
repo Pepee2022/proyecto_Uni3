44ef8ee76c1932cc1b1ed94bcbb74c7a1e82e868
Pepee1920 <017100363k@uandina.edu.pe>
2022-12-04 22:40:36 -0500

    Commit 04/12

--- db/models/tipoVenta.model.js
+++ db/models/tipoVenta.model.js
@@ -1,37 +1,35 @@
-const { Model, DataTypes, Sequelize } = require('sequelize');
+const { Model, DataTypes, Sequelize } = require("sequelize");
 
-const TIPOVENTA_TABLE = 'tipoventa';
+const TIPOVENTA_TABLE = "tipoventa";
 
 const tipoVentaSchema = {
-    id: {
-        primaryKey: true,
-        type: DataTypes.UUID
-    },
-    nombre: {
-        allowNull: false,
-        type: DataTypes.STRING
-    },
-    createdAt: {
-        allowNUll: false,
-        type: DataTypes.DATE,
-        field: 'created_at',
-        defaultValue: Sequelize.NOW
-    }
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    allowNUll: false,
+    type: DataTypes.DATE,
+    field: "created_at",
+    defaultValue: Sequelize.NOW,
+  },
 };
 
 class TipoVenta extends Model {
-    static associate() {
-        
-    }
+  static associate() {}
 
-    static config(sequelize) {
-        return {
-            sequelize,
-            tableName: TIPOVENTA_TABLE,
-            modelName: 'TipoVenta',
-            timestamps: false
-        };
-    }
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: TIPOVENTA_TABLE,
+      modelName: "TipoVenta",
+      timestamps: false,
+    };
+  }
 }
 
-module.exports = { TIPOVENTA_TABLE, tipoVentaSchema, TipoVenta};
+module.exports = { TIPOVENTA_TABLE, tipoVentaSchema, TipoVenta };
--- db/models/ventas.model.js
+++ db/models/ventas.model.js
@@ -1,49 +1,49 @@
-const { Model, DataTypes, Sequelize } = require('sequelize');
-const { TIPOVENTA_TABLE } = require('./tipoVenta.model');
+const { Model, DataTypes, Sequelize } = require("sequelize");
+const { TIPOVENTA_TABLE } = require("./tipoVenta.model");
 
-const VENTAS_TABLE = 'venta';
+const VENTAS_TABLE = "venta";
 
 const ventasSchema = {
-    id: {
-        primaryKey: true,
-        type: DataTypes.UUID
-    },
-    nombre: {
-        allowNull: false,
-        type: DataTypes.STRING
-    },
-    // tipoventaId: {
-    //     field: 'tipoventa_id',
-    //     allowNull: true,
-    //     type: DataTypes.UUID,
-    //     references: {
-    //         model: TIPOVENTA_TABLE,
-    //         key: 'id'
-    //     },
-    //     onUpdate: 'CASCADE',
-    //     onDelete: 'SET NULL'
-    // },
-    createdAt: {
-        allowNUll: false,
-        type: DataTypes.DATE,
-        field: 'created_at',
-        defaultValue: Sequelize.NOW
-    }
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  // tipoventaId: {
+  //     field: 'tipoventa_id',
+  //     allowNull: true,
+  //     type: DataTypes.UUID,
+  //     references: {
+  //         model: TIPOVENTA_TABLE,
+  //         key: 'id'
+  //     },
+  //     onUpdate: 'CASCADE',
+  //     onDelete: 'SET NULL'
+  // },
+  createdAt: {
+    allowNUll: false,
+    type: DataTypes.DATE,
+    field: "created_at",
+    defaultValue: Sequelize.NOW,
+  },
 };
 
 class Ventas extends Model {
-    static associate(models) {
-        this.belongsTo(models.TipoVenta, { as: 'tipoventa' });
-    }
+  static associate(models) {
+    this.belongsTo(models.TipoVenta, { as: "tipoventa" });
+  }
 
-    static config(sequelize) {
-        return {
-            sequelize,
-            tableName: VENTAS_TABLE,
-            modelName: 'Venta',
-            timestamps: false
-        };
-    }
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: VENTAS_TABLE,
+      modelName: "Venta",
+      timestamps: false,
+    };
+  }
 }
 
-module.exports = { VENTAS_TABLE, ventasSchema, Ventas};
+module.exports = { VENTAS_TABLE, ventasSchema, Ventas };
